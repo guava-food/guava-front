bf896a27f432b74ac13932a3d1a994096621a448
Leo Dan Peña <amouxaden@gmail.com>
2022-02-15 17:36:11 -0600

    wrapping up login screen
    Co-Authored-By: jcpalacios1056 <71674659+jcpalacios1056@users.noreply.github.com>
    Co-Authored-By: FrostyLz <78246014+FrostyLz@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(primarySwatch: Colors.grey),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -38,29 +38,62 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Button pushes:',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Helvetica Neue',
-                    fontSize: 36,
-                    fontWeight: FontWeight.w100)),
-            Text(
-              '$_counter',
-              style: TextStyle(
-                  fontFamily: 'Helvetica Neue',
-                  fontSize: 36,
-                  fontWeight: FontWeight.w100),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Image(
+                image: NetworkImage('https://i.imgur.com/Xr5Qp7j.png'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: 200.0,
+                    height: 30.0,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Login'),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: 200.0,
+                    height: 30.0,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Forgot Password'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
