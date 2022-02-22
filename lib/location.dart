5e5877f144b4785ac0ce9c40a3e67111a5538065
Leo Dan Peña <amouxaden@gmail.com>
2022-02-22 17:32:12 -0600

    scaffold for location return

--- lib/location.dart
+++ lib/location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({Key? key, required this.title}) : super(key: key);
@@ -13,12 +14,31 @@ class _LocationPageState extends State<LocationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome to Flutter'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Allow your location',
+            style: TextStyle(fontSize: 24),
+          ),
+          Text('We will need your location to find restaurants near you.'),
+          OutlinedButton(
+            onPressed: () {
+              //_determinePosition();
+            },
+            child: const Text('Allow location'),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('Skip'),
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('Cool button'),
+          ),
+        ],
+      )),
     );
   }
 }
