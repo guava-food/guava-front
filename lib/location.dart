962999e5961e01830284d5f2b566f3a0906bfc71
Leo Dan Peña <amouxaden@gmail.com>
2022-02-22 17:48:05 -0600

    finished location request

--- lib/location.dart
+++ lib/location.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -11,6 +13,60 @@ class LocationPage extends StatefulWidget {
 }
 
 class _LocationPageState extends State<LocationPage> {
+  double gps_latitude = -1.11;
+  double gps_longitude = -1.11;
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    print("succssfully allowed");
+    return await Geolocator.getCurrentPosition();
+  }
+
+  void _savePosition() async {
+    Position position = await _determinePosition();
+
+    setState(() {
+      gps_latitude = position.latitude.toDouble();
+      gps_longitude = position.longitude.toDouble();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,9 +90,13 @@ class _LocationPageState extends State<LocationPage> {
             child: const Text('Skip'),
           ),
           OutlinedButton(
-            onPressed: () {},
+            onPressed: () {
+              _savePosition();
+            },
             child: const Text('Cool button'),
           ),
+          Text(gps_latitude.toString()),
+          Text(gps_longitude.toString())
         ],
       )),
     );
